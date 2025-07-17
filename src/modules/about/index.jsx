87bbada0c1b0e5820a253430d1bf8d28d8b854a6
Pratik <pratik080.rejoice@gmail.com>
2025-07-17 17:37:31 +0530

    add: basics

--- src/modules/about/index.jsx
+++ src/modules/about/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function About() {
+  const [state, setState] = useState("black");
+  const colors = ["Red", "Green", 'Purple', "Blue", "Grey"]
+
   return (
     <div>
-      this is about page
+      {colors.filter((element, i, arr) => true).map((value, index, array) => (
+        <div key={index}>
+          <span onClick={() => value === 'Blue' ? null : setState(value)}>
+            {value}
+          </span>
+        </div>
+      ))}
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          border: "1px solid",
+          backgroundColor: state,
+        }}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
